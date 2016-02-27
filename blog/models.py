c6759ab1cfc23362098c2671827c85d5ee916b6f
knaznin <knaznin@broadway.com>
2016-02-27 17:40:43 -0600

    modified temp

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
 
+
+
 # Create your models here.
-class post(models.Model):
+class Post(models.Model):
     author= models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
